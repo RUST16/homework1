bb02f1beb178e772fd6231859edc29ac78cfce4a
RUST <oueee6@gmail.com>
2022-12-03 23:42:08 +0300

    First program complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа и выдает какое число большее, а какое меньшее
+
+ /*
+Console.Write("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if(number1 > number2)
+{
+    Console.WriteLine($"{number1} - max " );
+    Console.WriteLine($"{number2} - min ");
+}
+
+    else
+    {
+        Console.WriteLine($"{number2} - max " );
+        Console.WriteLine($"{number1} - min");
+    }
+
+*/
+
+//Задача 4. Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+/*
+Console.Write("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (number2 > number1)
+{
+     max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.Write($"{max} " );
+*/
