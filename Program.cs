d46422c0e7ce946c396338a44c4576a39e18201a
RUST <oueee6@gmail.com>
2022-12-04 00:10:39 +0300

    third program complete

--- Program.cs
+++ Program.cs
@@ -47,3 +47,19 @@ Console.Write($"max number: {max} ");
 
 */
 
+//Задача 6. Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+
+Console.Write("Imput testing number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write("Yes ");
+}
+
+else
+{
+    Console.Write("No ");
+}
+
+
